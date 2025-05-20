e1ac835c97e0b565d3906f023e2e88bfb002ff7b
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-05-20 21:46:22 +0500

    learning rust for project

---
+++ 8_structs/target/debug/deps/structs-67e1e912bfd72e10.d
@@ -0,0 +1,5 @@
+C:\Rust\8_structs\target\debug\deps\libstructs-67e1e912bfd72e10.rmeta: src\main.rs
+
+C:\Rust\8_structs\target\debug\deps\structs-67e1e912bfd72e10.d: src\main.rs
+
+src\main.rs:
---
+++ 8_structs/target/debug/deps/structs-905fdda640c708be.d
@@ -0,0 +1,5 @@
+C:\Rust\8_structs\target\debug\deps\libstructs-905fdda640c708be.rmeta: src\main.rs
+
+C:\Rust\8_structs\target\debug\deps\structs-905fdda640c708be.d: src\main.rs
+
+src\main.rs:
