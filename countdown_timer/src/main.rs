8ec23c1f5771ecd70fb150ff607a50226ffd911a
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-05-24 23:53:09 +0500

    count down optimzed

--- countdown_timer/src/main.rs
+++ countdown_timer/src/main.rs
@@ -2,12 +2,19 @@ use std::io;
 use std::thread;
 use std::time::Duration;
 fn main() {
-    let mut input = String::new();
-    print!("Enter amount of time");
-    io::stdin().read_line(&mut input).expect("Invalid input");
-
-    let timer: u16 = input.trim().parse().expect("Invalid number");
-    start_timer(timer);
+    loop {
+        let mut input = String::new();
+        println!("Enter amount of time");
+        io::stdin().read_line(&mut input).expect("Invalid input");
+        let timer: u16 = match input.trim().parse() {
+            Ok(timer) => timer,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+        start_timer(timer);
+    }
 }
 fn start_timer(timer: u16) {
     for i in (1..=timer).rev() {
