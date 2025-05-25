9a02dbe3a66720a88992a47ab151f4325e7d238a
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-05-26 01:24:53 +0500

    basics banking application completed

--- e_banking_application/src/main.rs
+++ e_banking_application/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 fn main() {
+    let mut user_balance: f64 = 0.0;
     loop {
         println!("Choose the option");
         println!("1. Deposit Money");
@@ -12,34 +13,66 @@ fn main() {
 
         io::stdin().read_line(&mut input).expect("input error");
 
-        let option: u8 = match input.trim().parse() {
-            Ok(option) => option,
+        let choice: u8 = match input.trim().parse() {
+            Ok(choice) => choice,
 
             Err(_) => {
                 println!("only numbers are accepted");
                 continue;
             }
         };
-
-        match option {
-            1 => {
-                print!("Money Deposited");
-                break;
-            }
-            2 => {
-                print!("Withdraw Money");
-                break;
-            }
-            3 => {
-                print!("Check Current Balance");
-                break;
-            }
-            4 => {
-                break;
-            }
-            _ => {
-                continue;
-            }
-        }
+        execute(choice, &mut user_balance);
     }
+}
+
+fn execute(choice: u8, user_balance: &mut f64) {
+    match choice {
+        1 => deposit(user_balance),
+        2 => withdraw(user_balance),
+        3 => fetch_balance(user_balance),
+        4 => exit(),
+        _ => println!("Wrong Choice, Try Again!"),
+    }
+}
+fn deposit(user_balance: &mut f64) {
+    let mut input = String::new();
+    println!("Please Enter Your Amount");
+    io::stdin().read_line(&mut input).expect("input error");
+
+    let amount: f64 = match input.trim().parse() {
+        Ok(choice) => choice,
+
+        Err(_) => {
+            println!("only numbers are accepted");
+            return;
+        }
+    };
+    *user_balance = *user_balance + amount;
+}
+
+fn withdraw(user_balance: &mut f64) {
+    let mut input = String::new();
+    println!("Please Withdrawal Amount");
+    io::stdin().read_line(&mut input).expect("input error");
+
+    let amount: f64 = match input.trim().parse() {
+        Ok(choice) => choice,
+
+        Err(_) => {
+            println!("only numbers are accepted");
+            return;
+        }
+    };
+    if amount <= *user_balance {
+        *user_balance = *user_balance - amount;
+    } else {
+        println!("Enter amount equal to or less than your balance")
+    }
+}
+fn fetch_balance(user_balance: &mut f64) {
+    println!("User current balance {}", *user_balance);
+}
+fn exit() {
+    println!("Thanks for using our service");
+    std::process::exit(0);
 }
